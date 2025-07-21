1cd3ebe778abaf3623772a1b6701fd606c6a6ddc
WesleyIntexBank <wesley.bezerra@intexbank.com.br>
2025-07-21 15:51:05 -0300

    K8S to AWS  -----  3

--- Terraform_AWS/main.tf
+++ Terraform_AWS/main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = { Service = "eks.amazonaws.com" },
       Action    = "sts:AssumeRole"
     }]
@@ -77,7 +77,7 @@ resource "aws_iam_role" "eks_node_group_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = { Service = "ec2.amazonaws.com" },
       Action    = "sts:AssumeRole"
     }]
